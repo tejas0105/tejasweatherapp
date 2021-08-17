070d4008663260f75f9e20f717b93555a8dc5e24
Tejas Pathak <54174816+tejas0105@users.noreply.github.com>
2021-08-17 11:53:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisisasecret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/weather'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 
 db = SQLAlchemy(app)
 
